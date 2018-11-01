48fc1a022964f0a53379250ed9e286e6066b3407
Arian Haliti <arian93haliti@gmail.com>
2018-11-01 16:22:36 +0100

    Add connection to db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
+const config = require("./config/config");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const port = process.PORT || 3000;
 const app = express();
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true }
+  )
+  .then(
+    () => {
+      console.log("Db connected");
+    },
+    err => console.log(err)
+  );
 
 app.get("/", (req, res) => {
   res.send("hello");
