0c75d5cb4519e961e791bcd7071a8d3ba0dfae75
Arian Haliti <arian93haliti@gmail.com>
2018-11-11 23:54:53 +0100

    Add model Post validation and route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
+// Load Post model
+const Post = require("../../models/Post");
+
+// Load Validator Posts
+const validatePostInput = require("../../validation/post");
 // @route    GET api/posts/test
 // @desc     Tests post route
 // @access   Public
@@ -9,5 +15,29 @@ router.get("/test", (req, res) => {
     msg: "posts works"
   });
 });
+
+// @route    Post api/posts
+// @desc     Tests post route
+// @access   Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    let { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) return res.status(400).json(errors);
+    let post = new Post({
+      user: req.user.id,
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar
+    });
+    post
+      .save()
+      .then(post => {
+        res.json(post);
+      })
+      .catch(e => res.status(400).json(e));
+  }
+);
 
 module.exports = router;
