7437486a7b3384e85a7d36acbaa7f74a827f8308
Arian Haliti <arian93haliti@gmail.com>
2018-11-12 20:29:13 +0100

    Add like/unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -85,5 +85,62 @@ router.delete(
       .catch(e => res.status(400).json({ message: "Could not find post" }));
   }
 );
+// @route    POST api/post/like/:id
+// @desc     Like a post
+// @access   Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (!post) return res.status(404).json("Post not found");
 
+        if (
+          post.likes.filter(value => value.user.toString() === req.user.id)
+            .length > 0
+        )
+          return res
+            .status(400)
+            .json({ alreadylike: "User already liked this post" });
+
+        // Add User id to likes array
+
+        post.likes.unshift({ user: req.user.id });
+
+        //Save it
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(e => res.status(404).json({ postnotfound: "Post not found" }));
+  }
+);
+// @route    POST api/post/unlike/:id
+// @desc     Unlike a post
+// @access   Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (!post) return res.status(404).json("Post not found");
+
+        if (
+          post.likes.filter(value => value.user.toString() === req.user.id)
+            .length == 0
+        )
+          return res.status(400).json({ notliked: "User has not voted" });
+
+        // Get remove index
+        let removeIndex = post.likes.findIndex(value => {
+          return value.user === req.user.id;
+        });
+        post.likes.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(e => res.status(404).json({ postnotfound: "Post not found" }));
+  }
+);
 module.exports = router;
